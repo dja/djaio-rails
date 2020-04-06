78ccd6711b1ad9646a9513549aa9b3f3dcb56ff3
Daniel Archer <me@dja.io>
2020-04-05 22:11:06 -0700

    Upgrade Rails to 6.0.2.2

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,19 @@
 require_relative 'boot'
 
-require 'rails'
+require "rails"
 # Pick the frameworks you want:
-require 'active_model/railtie'
-require 'active_job/railtie'
-require 'active_record/railtie'
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
-require 'action_view/railtie'
-require 'action_cable/engine'
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+# require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require "action_view/railtie"
+require "action_cable/engine"
 # require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,29 +21,17 @@ Bundler.require(*Rails.groups)
 
 module DjaioRails
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.0
+
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
 
     config.paths.add File.join('app', 'api'), glob: File.join('**', '*.rb')
     config.autoload_paths += Dir[Rails.root.join('app', 'api', '*')]
     config.autoload_paths += Dir[Rails.root.join('app', 'services', '**/')]
-
-    if ENV['RAILS_ENV'] == 'development'
-      config.middleware.insert_before 0, Rack::Cors do
-        allow do
-          origins '*'
-          resource '*', headers: :any, methods: [:get, :post, :options, :head]
-        end
-      end
-    else
-      config.middleware.insert_before 0, Rack::Cors do
-        allow do
-          origins /^https?:\/\/[w{3}]?.*dja.io$/, /^https?:\/\/[w{3}]?.*danieljacobarcher.com$/, /^https?:\/\/djaio.herokuapp.com$/, '199.38.176.0/22'
-          resource '*', headers: :any, methods: [:get, :post, :options, :head]
-        end
-      end
-    end
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
